f27623305a40a667c97e695a73c1c7dff05f7951
CutieCat2804 <cutiecattv@gmail.com>
2025-01-07 21:42:16 +0100

    wip add startpage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,23 @@
+import Header from "@/components/Header";
 import system from "@/theme";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider value={system}>
-      <Component {...pageProps} />
+      <Box backgroundColor="background">
+        <Header />
+        <Box
+          paddingY="12"
+          paddingX="8"
+          maxWidth="1400px"
+          height="100vh"
+          margin="auto"
+        >
+          <Component {...pageProps} />
+        </Box>
+      </Box>
     </ChakraProvider>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,43 @@
-import Header from "@/components/Header";
+import {
+  Button,
+  Grid,
+  GridItem,
+  Heading,
+  Highlight,
+  HStack,
+  Icon,
+  Text,
+} from "@chakra-ui/react";
+import { LuSchool } from "react-icons/lu";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <Grid templateColumns="repeat(5, 1fr)" gap="6">
+      <GridItem colSpan={3}>
+        <Grid rowGap="6">
+          <Heading size="6xl" color="text-primary">
+            <Highlight query="Lerne neue Skills" styles={{ color: "primary" }}>
+              Lerne neue Skills ganz einfach von zu Hause
+            </Highlight>
+          </Heading>
+          <Text fontSize="xl" color="text-secondary">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua.
+          </Text>
+          <HStack gap={4}>
+            <Button>Explore Now</Button>
+            <Button>Learn More</Button>
+          </HStack>
+        </Grid>
+      </GridItem>
+      <GridItem colSpan={2} margin="auto">
+        <Icon width="300px" height="300px" color="secondary">
+          <LuSchool />
+        </Icon>
+      </GridItem>
+    </Grid>
+  );
 }
